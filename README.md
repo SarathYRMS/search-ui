f3589d4d5f9e280525c2eecfe58171e5f51392c7
Sarath Yalla <Sarathchandra.Yalla@gmail.com>
2020-08-27 18:56:42 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # search-ui
+
+This is a Simple ReactJS Project which demonstrates the following
+
+ - Communicate with backend (search-service) application and display the results. The backend application will allow only localhost:3000 url otherwise it'll not interact with backend application.
+
+#### Application URL
+
+ - http://localhost:3000
+
+### Prerequisites
+
+#### Install Node JS
+
+Refer to https://nodejs.org/en/ to install nodejs
+
+Install create-react-app npm package globally. This will help to easily run the project and also build the source files easily. Use the following command to install create-react-app
+
+ - npm install -g create-react-app
+
+#### Cloning and Running the Application in local
+
+ - Step1: Clone the project from github (https://github.com/SarathYRMS/search-ui.git) into local
+    ###### git clone https://github.com/SarathYRMS/search-ui.git
+ - Step2: Install all the npm packages inorder to run the application. Before run the  below command make sure that you're in the project folder and type the following command to install all npm packages
+    ###### npm install
+    
+ - Step3: In order to run the application, type the following command
+    ###### npm start
+ - Step4: The Application Runs on localhost:3000
---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {value:'',data:[],firstLoad : true}
+
+    this._handleChange = this._handleChange.bind(this);
+  }
+
+  _handleChange(e){
+    this.setState({ value: e.target.value });
+  }
+  
+  _handleKeyDown = async (e) => {
+    if (e.key === 'Enter') {
+      const eventSource = await fetch('http://localhost:8080/search?q='+this.state.value)
+      const data = await eventSource.json()
+      this.setState({data : data,firstLoad : false})
+    } 
+
+  }
+
+  render() {
+      return (
+        <div>
+          <div className="container">
+            <label htmlFor="search">Search: </label>
+            <input id="search" className="searchBox" type="search" onChange={this._handleChange} onKeyDown={this._handleKeyDown} placeholder="for albums and books"></input>
+          </div>
+          <div className="container">
+          {this.state.data.length === 0 && !this.state.firstLoad && (
+              <div>No results found, please search with another term</div>
+          )}
+          {this.state.data.length > 0  && (
+            <table>
+              <thead>
+              <tr>
+                <th>Title</th>
+                <th>Authors</th>
+                <th>Type</th>
+              </tr>
+              </thead>
+              <tbody>
+              {this.state.data.map((item,idx) => {
+                return (
+                  <tr>
+                      <td>{item['title']}</td>
+                      <td>{item['authors'].join(",")}</td>
+                      <td>{item['type']}</td>
+                  </tr>
+                 )
+              })
+              }
+              </tbody>
+            </table>
+          )}
+          </div>
+        </div>
+      );
+  }
+}
+
+export default App;
